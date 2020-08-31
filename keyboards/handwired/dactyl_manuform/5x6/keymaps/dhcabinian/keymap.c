477d808f7713bb00972acda6a5fb25c67ee26300
David Cabinian <dcabinian@apple.com>
2020-08-30 23:26:14 -0700

    fix(layer lighting): fixed layer lighting

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dhcabinian/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dhcabinian/keymap.c
@@ -15,19 +15,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 const rgblight_segment_t PROGMEM movement_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {5, 4, HSV_CYAN},
 		{41, 4, HSV_CYAN}
 );
 const rgblight_segment_t PROGMEM settings_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {5, 5, HSV_PURPLE},
-		{41, 4, HSV_PURPLE}
+    {27, 4, HSV_PURPLE}
 );
 
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    movement_layer,    // Overrides caps lock layer
-    settings_layer     // Overrides other layers
+    movement_layer,
+    settings_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -38,7 +36,7 @@ void keyboard_post_init_user(void) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Both layers will light up if both kb layers are active
-    rgblight_set_layer_state(0, layer_state_cmp(state, 1));
+    rgblight_set_layer_state(0, layer_state_cmp(state, 1)); // layer_state_cmp finds if layer is active, index is into my_rgb_layers
     rgblight_set_layer_state(1, layer_state_cmp(state, 2));
     return state;
 }
